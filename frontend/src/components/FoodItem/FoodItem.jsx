51e1d4f441b2c42590a8d3c9da1797e89c86a3db
Tushar Konala <tusharkonala284@gmail.com>
2024-10-08 19:52:20 +0530

    first commit

---
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -0,0 +1,60 @@
+import React, { useContext, useState } from "react";
+import "./FoodItem.css";
+import { assets } from "../../assets/assets";
+import { StoreContext } from "../../Context/StoreContext";
+
+const FoodItem = ({ image, name, price, desc, id }) => {
+  const [itemCount, setItemCount] = useState(0);
+  const { cartItems, addToCart, removeFromCart, url, currency } =
+    useContext(StoreContext);
+
+  // Ensure cartItems is defined and handle undefined id case
+  const itemCountInCart = cartItems && cartItems[id] ? cartItems[id] : 0;
+
+  return (
+    <div className="food-item">
+      <div className="food-item-img-container">
+        <img
+          className="food-item-image"
+          src={`${url}/images/${image}`}
+          alt={name}
+        />
+        {!itemCountInCart ? (
+          <img
+            className="add"
+            onClick={() => addToCart(id)}
+            src={assets.add_icon_white}
+            alt="Add to cart"
+          />
+        ) : (
+          <div className="food-item-counter">
+            <img
+              src={assets.remove_icon_red}
+              onClick={() => removeFromCart(id)}
+              alt="Remove from cart"
+            />
+            <p>{itemCountInCart}</p>
+            <img
+              src={assets.add_icon_green}
+              onClick={() => addToCart(id)}
+              alt="Add to cart"
+            />
+          </div>
+        )}
+      </div>
+      <div className="food-item-info">
+        <div className="food-item-name-rating">
+          <p>{name}</p>
+          <img src={assets.rating_starts} alt="Rating stars" />
+        </div>
+        <p className="food-item-desc">{desc}</p>
+        <p className="food-item-price">
+          {currency}
+          {price}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default FoodItem;
